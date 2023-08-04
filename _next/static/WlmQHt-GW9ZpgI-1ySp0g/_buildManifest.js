8595fcc44d3baa94141868f6a8a338f0b2a3e818
Alex de Vries <alexthevries@gmail.com>
2023-08-04 15:59:54 +0200

    Updates

---
+++ _next/static/WlmQHt-GW9ZpgI-1ySp0g/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/574-477d4b324e89b5bc.js","static/css/a0b1586ae231c52a.css","static/chunks/pages/index-2e6f3748f31d0d44.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
